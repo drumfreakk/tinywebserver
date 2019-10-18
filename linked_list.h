3ca12556245916b2c7e123578da0ceb78f9dfc56
Kieran H <kieran.houtgraaf@gmail.com>
2019-10-18 17:03:07 +0200

    Node Lists work

--- linked_list.h
+++ linked_list.h
@@ -100,48 +100,139 @@ void printLinkedList(struct Linked_List *list){
 #endif
 #ifdef NODE
 
-struct Node{
-	char *key;
-	char *data;
+typedef struct Node{
+	int val;
 	struct Node *next;
-};
+} node_t;
 
+void print_list(node_t * head) {
+	node_t * current = head;
 
-
-void insertNew(struct Node *head, char *key, char *data){
-	struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
-//	newNode->key = malloc(sizeof(char) * 20);
-//	newNode->data = malloc(sizeof(char) * 20);
-//	strcpy(newNode->key, key);				//TODO Fix the sigabrt
-//	strcpy(newNode->data, data);
-
-	newNode->next = head;
-	head = newNode;
-//	printf("New: %x: %s\t-->\t%s\t@\t%x\n", head->next, head->key, head->data, head);
-}
-
-void removeHead(struct Node *head){
-	struct Node *newHead = head->next;
-//	free(head->key);
-//	free(head->data);
-	free(head);
-	head = newHead;
-}
-
-void removeList(struct Node *head){
-	while(head->next != NULL){
-		removeHead(head);
+	while (current != NULL) {
+		printf("%d\n", current->val);
+		current = current->next;
 	}
+	printf("\n");
 }
 
-void printList(struct Node *head){
-	struct Node *current = head;
-	while(current != NULL){
-//		printf("%x: %s\t-->\t%s\t\t%x\n", current, current->key, current->data, current->next);
-		printf("%x:%x\n", current, current->next);
-//		printf("%s\t-->\t%s\n", current->key, current->data);
-		current = current->next;	//TODO fix double last
+int init_list(node_t ** head, int val){
+	node_t *new_node = NULL;
+	new_node = malloc(sizeof(node_t));
+	if(new_node == NULL){
+		return -1;
 	}
+	new_node->val = val;
+	*head = new_node;
+}
+
+int add_last(node_t * head, int val) {
+	node_t * current = head;
+	while (current->next != NULL) {
+		current = current->next;
+	}
+
+	/* now we can add a new variable */
+	current->next = malloc(sizeof(node_t));
+	if(current->next == NULL){
+		return -1;
+	}
+	current->next->val = val;
+	current->next->next = NULL;
+}
+
+int push(node_t ** head, int val) {
+	node_t * new_node;
+	new_node = malloc(sizeof(node_t));
+	if(new_node == NULL){
+		return -1;
+	}
+
+	new_node->val = val;
+	new_node->next = *head;
+	*head = new_node;
+}
+
+int pop(node_t ** head) {
+	int retval = -1;
+	node_t * next_node = NULL;
+
+	if (*head == NULL) {
+		return -1;
+	}
+
+	next_node = (*head)->next;
+	retval = (*head)->val;
+	free(*head);
+	*head = next_node;
+
+	return retval;
+}
+
+int remove_last(node_t * head) {
+	int retval = 0;
+	/* if there is only one item in the list, remove it */
+	if (head->next == NULL) {
+		retval = head->val;
+		free(head);
+		return retval;
+	}
+
+	/* get to the second to last node in the list */
+	node_t * current = head;
+	while (current->next->next != NULL) {
+		current = current->next;
+	}
+
+	/* now current points to the second to last item of the list, so let's remove current->next */
+	retval = current->next->val;
+	free(current->next);
+	current->next = NULL;
+	return retval;
+
+}
+
+int remove_by_index(node_t ** head, int n) {
+	int i = 0;
+	int retval = -1;
+	node_t * current = *head;
+	node_t * temp_node = NULL;
+
+	if (n == 0) {
+		return pop(head);
+	}
+
+	for (i = 0; i < n-1; i++) {
+		if (current->next == NULL) {
+			return -1;
+		}
+		current = current->next;
+	}
+
+	temp_node = current->next;
+	retval = temp_node->val;
+	current->next = temp_node->next;
+	free(temp_node);
+
+	return retval;
+
+}
+
+int remove_by_value(node_t ** head, int val){
+	node_t * current = *head;
+	int count = 0;
+
+	while (current != NULL) {
+		if(current->val == val){
+			return remove_by_index(head, count);
+		}
+		count++;
+		current = current->next;
+	}
+	return -1;
+}
+
+void clear_list(node_t **head){
+	while(pop(head) != -1){}
 }
 
 #endif
--- main.c
+++ main.c
@@ -10,241 +10,221 @@
 #include "hacking-network.h"
 #include "linked_list.h"
 
-//struct Test{
-//	int s;
-////	int *f;
-//};
-//
-//void test(struct Test *a){
-//	printf("%d\n", a->s);
-////	printf("%d\n", (int)a->f);
-//	a->s = 7;
-//}
-//
-//void testN(struct Test *a){
-////	a = malloc(sizeof(struct Test));
-//	a->s = 8;
-//}
-//
-//int main(void){
-////	struct Node *head = NULL;
-////	insertNew(head, "Key", "Data");
-//////	insertNew(head, "Key2", "Data2");
-////	removeHead(head);
-////	insertNew(head, "Key3", "Data3");
-////	printList(head);
-////	removeList(head);
-//
-//int z = 2;
-////	struct Test g;
-////	g.s = 4;
-////	g.f = z;
-//	struct Test d;
-////	d = &g;
-//
-//
-////	test(d);
-////	printf("%d\n", d->s);
-//	testN(&d);
-//z = 6;
-////	d->f = z;
-//	test(&d);
-//	d.s = 9;
-//	test(&d);
-////	free(&d);
-//
-//	return 0;
-//}
+int main(void){
+	node_t * head = NULL;
+	init_list(&head, 1);
+	print_list(head);
+	push(&head, 0);
+	print_list(head);
+	add_last(head, 2);
+	add_last(head, 3);
+	add_last(head, 4);
+	add_last(head, 5);
+	add_last(head, 6);
+	add_last(head, 7);
+	print_list(head);
+	printf("Pop:%d\n\n", pop(&head));
+	print_list(head);
+	printf("Last:%d\n\n", remove_last(head));
+	print_list(head);
+	printf("Index:%d\n\n", remove_by_index(&head, 2));
+	print_list(head);
+	printf("Value:%d\n\n", remove_by_value(&head, 8));
+	print_list(head);
+	clear_list(&head);
 
-
-
-#define PORT 8080   // the port users will be connecting to
-#define WEBROOT "./webroot" // the web server's root directory
-
-void handle_connection(int, struct sockaddr_in *); // handle web requests
-int get_file_size(int); // returns the filesize of open file descriptor
-
-int main(void) {
-
-//	struct Linked_List list;
-//
-//	init_list(&list, 5);
-//	strcpy(list.keys[0], "hello");
-//	strcpy(list.keys[1], "qwertyuiopasdfghjklzxcv");
-//	printf("keyA: %s\tkeyB: %s\tkeyC: %s\n", list.keys[0], list.keys[1], list.keys[2]);
-//
-//
-//	end(&list);
-//	return 0;
-
-	int sockfd, new_sockfd, yes=1;
-	struct sockaddr_in host_addr, client_addr;   // my address information
-	socklen_t sin_size;
-
-	printf("Accepting web requests on port %d\n", PORT);
-
-	if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) == -1)
-		fatal("in socket");
-
-	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
-		fatal("setting socket option SO_REUSEADDR");
-
-	host_addr.sin_family = AF_INET;      // host byte order
-	host_addr.sin_port = htons(PORT);    // short, network byte order
-	host_addr.sin_addr.s_addr = INADDR_ANY; // automatically fill with my IP
-	memset(&(host_addr.sin_zero), '\0', 8); // zero the rest of the struct
-
-	if (bind(sockfd, (struct sockaddr *)&host_addr, sizeof(struct sockaddr)) == -1)
-		fatal("binding to socket");
-
-	if (listen(sockfd, 20) == -1)
-		fatal("listening on socket");
-
-	while(1) {   // Accept loop
-		sin_size = sizeof(struct sockaddr_in);
-		new_sockfd = accept(sockfd, (struct sockaddr *)&client_addr, &sin_size);
-		if(new_sockfd == -1)
-			fatal("accepting connection");
-
-		handle_connection(new_sockfd, &client_addr);
-	}
 	return 0;
 }
 
-/* This function handles the connection on the passed socket from the
- * passed client address.  The connection is processed as a web request
- * and this function replies over the connected socket.  Finally, the
- * passed socket is closed at the end of the function.
- */
-void handle_connection(int sockfd, struct sockaddr_in *client_addr_ptr) {
-	unsigned char *ptr, request[500], resource[500], header[500], content[500];
-	int fd, length, contentLength;
-	enum HTTPType type;
-#ifdef LIST
-	struct Linked_List headers;
-	init_list(&headers, 20);
-#endif
 
-	length = recv_line(sockfd, request);
-	//dump(request, length);
-	printf("Got request from %s:%d \"%s\"\n", inet_ntoa(client_addr_ptr->sin_addr), ntohs(client_addr_ptr->sin_port), request);
-
-	ptr = strstr(request, " HTTP/"); // search for valid looking request
-	if(ptr == NULL) { // then this isn't valid HTTP
-		printf(" NOT HTTP!\n");
-	} else {
-		*ptr = 0; // terminate the buffer at the end of the URL
-		ptr = NULL; // set ptr to NULL (used to flag for an invalid request)
-		if(strncmp(request, "GET ", 4) == 0){  // get request
-			ptr = request + 4; // ptr is the URL
-			type = GET;
-		}
-		if(strncmp(request, "HEAD ", 5) == 0){ // head request
-			ptr = request + 5; // ptr is the URL
-			type = HEAD;
-		}
-		if(strncmp(request, "POST ", 5) == 0){
-			ptr = request + 5;
-			type = POST;
-
-			char value[100];
-			int i, matched = 0, count = 0;
-			while(recv_line(sockfd, header)){
-#ifdef LIST
-				if(headers.size -2 == count){
-					resize(&headers, count+10);
-				}
-#endif
-
-				for(i = 0; i < strlen(header); i++){
-//					printf("Header[%d]: %c\n", i, header[i]);
-					if(header[i] == ": "[matched]){
-						matched++;
-//						printf("I:%d\n", i);
-						if(matched == 2){
-//							printf("%d\n", i);
-							value[i-1] = '\0';
-#ifdef NODE
-							insertNew(value, &header[i + 1]);
-#endif
-#ifdef LIST
-							strcpy(headers.keys[count], value);		//TODO: only use keys.value[]
-							strcpy(headers.values[count], &header[i+1]);
-#endif
-							break;
-						}
-					}else{
-						matched = 0;
-					}
-					value[i] = header[i];
-				}
-//				printf("I2:%d\n", i);
-//				dump(header, strlen(header)+1);
-//				dump(value, strlen(value)+1);
-//				printf("%s\n", &header[i+1]);
-
-				count++;
-			}
-#ifdef LIST
-			printLinkedList(&headers);
-#endif
-#ifdef NODE
-			printList();
-#endif
-
-//			contentLength = atoi(getValueByKey(&headers, "Content-Length"));
-//			recv(sockfd, content, contentLength, 0);
-//			dump(content, contentLength);
-//			printf("Content-Length: %d\nContent: %s\n", contentLength, content);	//TODO fix double free or corruption(out)(free linked list?)
-		}
-		if(ptr == NULL) { // then this is not a recognized request
-			printf("\tUNKNOWN REQUEST!\n");
-		} else { // valid request, with ptr pointing to the resource name
-			if (ptr[strlen(ptr) - 1] == '/')  // for resources ending with '/'
-				strcat(ptr, "index.html");     // add 'index.html' to the end
-			strcpy(resource, WEBROOT);     // begin resource with web root path
-			strcat(resource, ptr);         //  and join it with resource path
-			fd = open(resource, O_RDONLY, 0); // try to open the file
-			printf("\tOpening \'%s\'\t", resource);
-			if(fd == -1) { // if file is not found
-				printf(" 404 Not Found\n");
-				send_string(sockfd, "HTTP/1.0 404 NOT FOUND\r\n");
-				send_string(sockfd, "Server: Tiny webserver\r\n\r\n");
-				send_string(sockfd, "<html><head><title>404 Not Found</title></head>");
-				send_string(sockfd, "<body><h1>URL not found</h1></body></html>\r\n");
-			} else {      // otherwise, serve up the file
-				printf(" 200 OK\n");
-				send_string(sockfd, "HTTP/1.0 200 OK\r\n");
-				send_string(sockfd, "Server: Tiny webserver\r\n\r\n");
-				if(type == GET || type == POST) { // then this is a GET request
-					if( (length = get_file_size(fd)) == -1)
-						fatal("getting resource file size");
-					if( (ptr = (unsigned char *) malloc(length)) == NULL)
-						fatal("allocating memory for reading resource");
-					read(fd, ptr, length); // read the file into memory	//TODO:
-					send(sockfd, ptr, length, 0);  // send it to socket
-					free(ptr); // free file memory
-				}
-				close(fd); // close the file		//TODO:
-			} // end if block for file found/not found
-		} // end if block for valid request
-	} // end if block for valid HTTP
-	shutdown(sockfd, SHUT_RDWR); // close the socket gracefully
-#ifdef NODE
-	removeList();
-#endif
-#ifdef LIST
-	end(&headers);
-#endif
-}
-
-/* This function accepts an open file descriptor and returns
- * the size of the associated file.  Returns -1 on failure.
- */
-int get_file_size(int fd) {
-	struct stat stat_struct;
-
-	if(fstat(fd, &stat_struct) == -1)
-		return -1;
-	return (int) stat_struct.st_size;
-}
+//
+//#define PORT 8080   // the port users will be connecting to
+//#define WEBROOT "./webroot" // the web server's root directory
+//
+//void handle_connection(int, struct sockaddr_in *); // handle web requests
+//int get_file_size(int); // returns the filesize of open file descriptor
+//
+//int main(void) {
+//
+////	struct Linked_List list;
+////
+////	init_list(&list, 5);
+////	strcpy(list.keys[0], "hello");
+////	strcpy(list.keys[1], "qwertyuiopasdfghjklzxcv");
+////	printf("keyA: %s\tkeyB: %s\tkeyC: %s\n", list.keys[0], list.keys[1], list.keys[2]);
+////
+////
+////	end(&list);
+////	return 0;
+//
+//	int sockfd, new_sockfd, yes=1;
+//	struct sockaddr_in host_addr, client_addr;   // my address information
+//	socklen_t sin_size;
+//
+//	printf("Accepting web requests on port %d\n", PORT);
+//
+//	if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) == -1)
+//		fatal("in socket");
+//
+//	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
+//		fatal("setting socket option SO_REUSEADDR");
+//
+//	host_addr.sin_family = AF_INET;      // host byte order
+//	host_addr.sin_port = htons(PORT);    // short, network byte order
+//	host_addr.sin_addr.s_addr = INADDR_ANY; // automatically fill with my IP
+//	memset(&(host_addr.sin_zero), '\0', 8); // zero the rest of the struct
+//
+//	if (bind(sockfd, (struct sockaddr *)&host_addr, sizeof(struct sockaddr)) == -1)
+//		fatal("binding to socket");
+//
+//	if (listen(sockfd, 20) == -1)
+//		fatal("listening on socket");
+//
+//	while(1) {   // Accept loop
+//		sin_size = sizeof(struct sockaddr_in);
+//		new_sockfd = accept(sockfd, (struct sockaddr *)&client_addr, &sin_size);
+//		if(new_sockfd == -1)
+//			fatal("accepting connection");
+//
+//		handle_connection(new_sockfd, &client_addr);
+//	}
+//	return 0;
+//}
+//
+///* This function handles the connection on the passed socket from the
+// * passed client address.  The connection is processed as a web request
+// * and this function replies over the connected socket.  Finally, the
+// * passed socket is closed at the end of the function.
+// */
+//void handle_connection(int sockfd, struct sockaddr_in *client_addr_ptr) {
+//	unsigned char *ptr, request[500], resource[500], header[500], content[500];
+//	int fd, length, contentLength;
+//	enum HTTPType type;
+//#ifdef LIST
+//	struct Linked_List headers;
+//	init_list(&headers, 20);
+//#endif
+//
+//	length = recv_line(sockfd, request);
+//	//dump(request, length);
+//	printf("Got request from %s:%d \"%s\"\n", inet_ntoa(client_addr_ptr->sin_addr), ntohs(client_addr_ptr->sin_port), request);
+//
+//	ptr = strstr(request, " HTTP/"); // search for valid looking request
+//	if(ptr == NULL) { // then this isn't valid HTTP
+//		printf(" NOT HTTP!\n");
+//	} else {
+//		*ptr = 0; // terminate the buffer at the end of the URL
+//		ptr = NULL; // set ptr to NULL (used to flag for an invalid request)
+//		if(strncmp(request, "GET ", 4) == 0){  // get request
+//			ptr = request + 4; // ptr is the URL
+//			type = GET;
+//		}
+//		if(strncmp(request, "HEAD ", 5) == 0){ // head request
+//			ptr = request + 5; // ptr is the URL
+//			type = HEAD;
+//		}
+//		if(strncmp(request, "POST ", 5) == 0){
+//			ptr = request + 5;
+//			type = POST;
+//
+//			char value[100];
+//			int i, matched = 0, count = 0;
+//			while(recv_line(sockfd, header)){
+//#ifdef LIST
+//				if(headers.size -2 == count){
+//					resize(&headers, count+10);
+//				}
+//#endif
+//
+//				for(i = 0; i < strlen(header); i++){
+////					printf("Header[%d]: %c\n", i, header[i]);
+//					if(header[i] == ": "[matched]){
+//						matched++;
+////						printf("I:%d\n", i);
+//						if(matched == 2){
+////							printf("%d\n", i);
+//							value[i-1] = '\0';
+//#ifdef NODE
+//							insertNew(value, &header[i + 1]);
+//#endif
+//#ifdef LIST
+//							strcpy(headers.keys[count], value);		//TODO: only use keys.value[]
+//							strcpy(headers.values[count], &header[i+1]);
+//#endif
+//							break;
+//						}
+//					}else{
+//						matched = 0;
+//					}
+//					value[i] = header[i];
+//				}
+////				printf("I2:%d\n", i);
+////				dump(header, strlen(header)+1);
+////				dump(value, strlen(value)+1);
+////				printf("%s\n", &header[i+1]);
+//
+//				count++;
+//			}
+//#ifdef LIST
+//			printLinkedList(&headers);
+//#endif
+//#ifdef NODE
+//			printList();
+//#endif
+//
+////			contentLength = atoi(getValueByKey(&headers, "Content-Length"));
+////			recv(sockfd, content, contentLength, 0);
+////			dump(content, contentLength);
+////			printf("Content-Length: %d\nContent: %s\n", contentLength, content);	//TODO fix double free or corruption(out)(free linked list?)
+//		}
+//		if(ptr == NULL) { // then this is not a recognized request
+//			printf("\tUNKNOWN REQUEST!\n");
+//		} else { // valid request, with ptr pointing to the resource name
+//			if (ptr[strlen(ptr) - 1] == '/')  // for resources ending with '/'
+//				strcat(ptr, "index.html");     // add 'index.html' to the end
+//			strcpy(resource, WEBROOT);     // begin resource with web root path
+//			strcat(resource, ptr);         //  and join it with resource path
+//			fd = open(resource, O_RDONLY, 0); // try to open the file
+//			printf("\tOpening \'%s\'\t", resource);
+//			if(fd == -1) { // if file is not found
+//				printf(" 404 Not Found\n");
+//				send_string(sockfd, "HTTP/1.0 404 NOT FOUND\r\n");
+//				send_string(sockfd, "Server: Tiny webserver\r\n\r\n");
+//				send_string(sockfd, "<html><head><title>404 Not Found</title></head>");
+//				send_string(sockfd, "<body><h1>URL not found</h1></body></html>\r\n");
+//			} else {      // otherwise, serve up the file
+//				printf(" 200 OK\n");
+//				send_string(sockfd, "HTTP/1.0 200 OK\r\n");
+//				send_string(sockfd, "Server: Tiny webserver\r\n\r\n");
+//				if(type == GET || type == POST) { // then this is a GET request
+//					if( (length = get_file_size(fd)) == -1)
+//						fatal("getting resource file size");
+//					if( (ptr = (unsigned char *) malloc(length)) == NULL)
+//						fatal("allocating memory for reading resource");
+//					read(fd, ptr, length); // read the file into memory	//TODO:
+//					send(sockfd, ptr, length, 0);  // send it to socket
+//					free(ptr); // free file memory
+//				}
+//				close(fd); // close the file		//TODO:
+//			} // end if block for file found/not found
+//		} // end if block for valid request
+//	} // end if block for valid HTTP
+//	shutdown(sockfd, SHUT_RDWR); // close the socket gracefully
+//#ifdef NODE
+//	removeList();
+//#endif
+//#ifdef LIST
+//	end(&headers);
+//#endif
+//}
+//
+///* This function accepts an open file descriptor and returns
+// * the size of the associated file.  Returns -1 on failure.
+// */
+//int get_file_size(int fd) {
+//	struct stat stat_struct;
+//
+//	if(fstat(fd, &stat_struct) == -1)
+//		return -1;
+//	return (int) stat_struct.st_size;
+//}
